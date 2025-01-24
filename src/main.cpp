cd77646d3b0e72d7f724653537f26c8dc75648fe
David Petry <petry103@gmail.com>
2025-01-24 19:33:31 +0100

    feat: low power test with new board

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ PA2     RST
 PA1     BSY
 
 PC0     DCF_SIG
-PC1     DCF_PWR 
+PC1     DCF_PWR
 
 PC3     LED
 
@@ -37,19 +37,52 @@ void write_string_at(char* string, uint8_t string_length, uint8_t x, uint8_t y);
 void number_to_string(int number, char *result, uint8_t result_length);
 void setX(uint8_t x);
 void setY(uint8_t y);
+void init_TCB();
+void init_rtc();
+
+struct tm *end_time;
+struct tm *current_time;
 
 bool get_data;
 void write_72_at(uint8_t number, uint8_t x, uint8_t y);
 
+char data[60];
+uint8_t data_count;
+bool get_time;
+unsigned millis;
+bool state, last_state;
+uint8_t second, minute, hour;
+
 int main() {
+    _PROTECTED_WRITE(CLKCTRL_MCLKCTRLB, 0x09); // use protected write to set F_CPU to 625KHz
+    PORTC_DIRSET = PIN3_bm;
+    _PROTECTED_WRITE(CLKCTRL_XOSC32KCTRLA, 0x03);
+    for (int i = 0; i < 8; i++) {
+      (&PORTA_PIN0CTRL)[i] = PORT_ISC_INPUT_DISABLE_gc | PORT_PULLUPEN_bm;
+      (&PORTB_PIN0CTRL)[i] = PORT_ISC_INPUT_DISABLE_gc | PORT_PULLUPEN_bm;
+      (&PORTC_PIN0CTRL)[i] = PORT_ISC_INPUT_DISABLE_gc | PORT_PULLUPEN_bm;
+    }
+    PORTC_PIN0CTRL = PORT_ISC_INPUT_DISABLE_gc;
+    PORTC_PIN3CTRL = PORT_ISC_INPUT_DISABLE_gc;
+    PORTB_PIN4CTRL = PORT_ISC_INPUT_DISABLE_gc;
+    while (!(CLKCTRL_MCLKSTATUS & PIN5_bm)) {
+    }
+    init_rtc();
+    while (1){
+      SLPCTRL_CTRLA = SLPCTRL_SEN_bm | SLPCTRL_SMODE_STDBY_gc;
+      __asm__ __volatile__ ( "sleep" "\n\t" :: );
+    }
+    /*init_TCB();
+    data_count = 0;
+    get_time = true;
     init();
-    
+
     char test[20];
-      
+
     write_72_at(0, 0, 0);
     write_72_at(1, 1, 0);
     write_72_at(2, 2, 0);
-    write_72_at(3, 3, 0);    
+    write_72_at(3, 3, 0);
 
     update();
     standby();
@@ -61,17 +94,18 @@ int main() {
         PORTB_OUTCLR = PIN4_bm;
         _delay_ms(1000);
     }
+    */
     return 0;
 }
 
 void sda(uint8_t val) {
-    if (val) 
+    if (val)
         PORTA_OUTSET = PIN6_bm;
     else
         PORTA_OUTCLR = PIN6_bm;
 }
 void scl(uint8_t val) {
-    if (val) 
+    if (val)
         PORTA_OUTSET = PIN5_bm;
     else
         PORTA_OUTCLR = PIN5_bm;
@@ -88,7 +122,7 @@ void write_byte(uint8_t data) {
         data = data << 1;
     }
     PORTA_OUTSET = PIN4_bm; //cs 1
-   
+
 }
 void send_cmd(uint8_t cmd) {
     PORTA_OUTCLR = PIN3_bm; //dc 0
@@ -108,7 +142,7 @@ void init() {
 
     _delay_ms(10);
   //  PORTB_OUTCLR = PIN3_bm; //scl low ?
-    
+
     send_cmd(0x12);
     _delay_ms(20);
 
@@ -116,7 +150,7 @@ void init() {
     send_data(0xc7);
     send_data(0);
     send_data(0);
-    
+
     send_cmd(0x11);
     send_data(1);
 
@@ -132,14 +166,14 @@ void init() {
 
     send_cmd(0x1A);
     send_data(0);
-    
-    send_cmd(0x22);   
+
+    send_cmd(0x22);
     send_data(0xb1);
 
     send_cmd(0x20);
     while (PORTA_IN & PIN1_bm) //bsy
         _delay_ms(10);
-    
+
     send_cmd(0x4e);
     send_data(0x00);
 
@@ -180,9 +214,9 @@ void write_char_at(char c, uint8_t x, uint8_t y) {
     for(int i = 0; i < 10; i ++) {
         setY(2 * y);
         setX(i + 10 * x);
-        send_cmd(0x24);        
+        send_cmd(0x24);
         send_data(~font[c][i*2+1]);
-        send_data(~font[c][i*2]);         
+        send_data(~font[c][i*2]);
     }
 }
 void write_string_at(char* string, uint8_t string_length, uint8_t x, uint8_t y) {
@@ -191,7 +225,7 @@ void write_string_at(char* string, uint8_t string_length, uint8_t x, uint8_t y) 
     }
     for (int i = 0; i < string_length; i++) {
         write_char_at(string[i], x + i, y);
-    } 
+    }
 }
 
 void setX(uint8_t x) {
@@ -208,8 +242,8 @@ uint16_t scale_byte(uint8_t in) {
     uint16_t res = 0;
     for (int i = 0; i < 8; i++) {
         if (in & 1 << i) {
-            res |= 1 << (i*2) | 1 << (i*2 + 1);    
-        }      
+            res |= 1 << (i*2) | 1 << (i*2 + 1);
+        }
     }
     return res;
 }
@@ -221,18 +255,18 @@ void write_large_char_at(char c, uint8_t x, uint8_t y) {
         msb = scale_byte(~font[c][i*2]);
         setY(y);
         setX(2 * i + 11 * x);
-        send_cmd(0x24);        
+        send_cmd(0x24);
         send_data(lsb>>8);
         send_data(lsb & 0xFF);
-        send_data(msb>>8);         
-        send_data(msb & 0xFF); 
-        setY(y);  
+        send_data(msb>>8);
+        send_data(msb & 0xFF);
+        setY(y);
         setX(2 * i + 11 * x + 1);
-        send_cmd(0x24);        
+        send_cmd(0x24);
         send_data(lsb>>8);
         send_data(lsb & 0xFF);
-        send_data(msb>>8);         
-        send_data(msb & 0xFF);        
+        send_data(msb>>8);
+        send_data(msb & 0xFF);
     }
 }
 
@@ -264,8 +298,104 @@ void write_72_at(uint8_t number, uint8_t x, uint8_t y) {
         setY(9 * y);
         send_cmd(0x24);
         for (int j = 0; j < 9; j++) {
-            
+
             send_data(~calibri_72ptBitmaps[number * 432 + j + 9 * i]);
         }
     }
+}
+
+ISR(TCB0_INT_vect) {
+  cli();
+  TCB0_INTFLAGS |= TCB_CAPT_bm;
+  TCB0_CNT = 0;
+
+  if (!get_time) {}
+  else {
+    millis += 10;
+    if (PORTC_IN & PIN2_bm) {
+      state = 1;
+    }
+    else {
+      state = 0;
+    }
+    if (!(state == last_state)) {
+      PORTB_OUTTGL = PIN4_bm;
+      if (!state) {                     //just went low
+        if ((millis > 50) && (millis < 145)) {
+          data[data_count] = 0;
+          data_count++;
+        }
+        else if ((millis > 150) && (millis < 245)) {
+          data[data_count] = 1;
+          data_count++;
+        }
+        else {
+          data_count = 0;                    //bit not recognized
+        }
+
+      }
+      if (state) {
+        if (millis > 1500) {  	        //start detected
+          if (data_count == 59) {            //full data received, try to analyze
+            int old_time = minute * 60 + second + hour * 3600;
+            minute=data[27]*40 + data[26]*20 + data[25]*10 + data[24]*8 + data[23]*4 + data[22]*2 + data[21];
+            hour=data[34]*20 + data[33]*10 + data[32]*8 + data[31]*4 + data[30]*2 + data[29];
+            second = 0;
+            /*
+            if (!first_time) {
+              if (old_time < (hour * 3600 + minute * 60)) {
+                while (RTC_STATUS & RTC_CMPBUSY_bm);
+                RTC_CMP = RTC_CMP - 1;
+              }
+              else if (old_time > (hour * 3600 + minute * 60)) {
+                while (RTC_STATUS & RTC_CMPBUSY_bm);
+                RTC_CMP = RTC_CMP + 1;
+              }
+            }
+            */
+            get_time = false;
+            PORTC_OUTCLR = PIN3_bm;
+            PORTB_OUTCLR = PIN4_bm;
+            while (RTC_STATUS & RTC_CNTBUSY_bm) ;
+            RTC_CNT = 0;
+            TCB0_CTRLA = 0;
+          }
+          data_count = 0;
+        }
+      }
+      millis = 0;
+      last_state = state;
+    }
+  }
+  sei();
+}
+
+ISR(RTC_CNT_vect) {
+  cli();
+  RTC_INTFLAGS |= RTC_CMP_bm;
+  RTC_CNT = 0;
+  PORTC_OUTTGL = PIN3_bm;
+  sei();
+}
+
+void init_TCB() {
+  //runs of main clock /2
+  //runs in standby
+  TCB0_CTRLA = TCB_RUNSTDBY_bm | TCB_CLKSEL_CLKDIV2_gc | TCB_ENABLE_bm;
+  TCB0_CCMP = 3124; // interrupt every 10ms
+  TCB0_INTCTRL = TCB_CAPT_bm;
+}
+
+void init_rtc(){
+  cli();
+
+  _delay_ms(10);
+  while ((RTC_STATUS & RTC_CTRLABUSY_bm) || (RTC_PITSTATUS & RTC_CTRLBUSY_bm)) ;
+
+  RTC_CLKSEL = RTC_CLKSEL_TOSC32K_gc;
+  RTC_CTRLA = RTC_RTCEN_bm | RTC_RUNSTDBY_bm;					      // enable RTC
+  RTC_INTCTRL = RTC_CMP_bm;
+  while (RTC_STATUS & RTC_PERBUSY_bm);
+  RTC_CMP = 32768;
+  sei();
 }
